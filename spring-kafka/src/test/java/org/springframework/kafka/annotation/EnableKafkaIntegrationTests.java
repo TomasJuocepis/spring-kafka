ba8a06fc57c6bf0063a865885f97a808c80802a0
Gary Russell <grussell@pivotal.io>
2016-03-10 09:14:42 -0500

    More Polish

--- spring-kafka/src/test/java/org/springframework/kafka/annotation/EnableKafkaIntegrationTests.java
+++ spring-kafka/src/test/java/org/springframework/kafka/annotation/EnableKafkaIntegrationTests.java
@@ -225,10 +225,10 @@ public class EnableKafkaIntegrationTests {
 			this.latch4.countDown();
 		}
 
-		@KafkaListener(id = "fiz", topicPartitions =
-				{@TopicPartition(topic = "annotated5", partitions = {"0", "1"}),
-				 @TopicPartition(topic = "annotated6", partitions = {"0", "1"})
-				})
+		@KafkaListener(id = "fiz", topicPartitions = {
+				@TopicPartition(topic = "annotated5", partitions = {"0", "1"}),
+				@TopicPartition(topic = "annotated6", partitions = {"0", "1"})
+			})
 		public void listen5(ConsumerRecord<?, ?> record) {
 			this.record = record;
 			this.latch5.countDown();
